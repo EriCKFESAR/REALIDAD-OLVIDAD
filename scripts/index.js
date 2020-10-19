149dc50d3ffcc6d9d5760a1a356739ffc1f6284e
Erick Peña <nero2204.gs@gmail.com>
2020-10-19 14:06:19 -0500

    version 0.9.5

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,36 @@
-const masInfo = document.getElementById('mas_informacion');
+const   masInfo = document.getElementById('mas_informacion'),
+        carrusel = document.querySelector('.thumbnail-container'),
+
+        rutaObras = 'resours/obras/';
+
+/* <Peticion a Json> */
+(async function getArtista(){
+    const requestURL = 'json/indice.json'
+    try {
+        const response = await fetch(requestURL);
+        data = await response.json();
+        loadContent();
+        loadCarousel();
+    } catch (error) {
+        console.log(error);
+    }
+})();
+/* </Peticion a Json> */
+
+/* <Carga de contenido principal en documento html> */
+function loadContent(){
+    data.forEach(obra => {
+        carrusel.innerHTML +=
+        `<div class="item fx-around fx-column">
+            <a class="d-contents" href="single.html?artista=${obra.id}">
+                <h3 class="txt-capitalize">${obra.titulo}</h3>
+                <img src="${rutaObras}${obra.imagen}" alt="obra-1">
+                <p maxlength="20"> ${obra.artista}</p>
+            </a>
+        </div>`
+    });
+}
+/* </Carga de contenido principal en documento html> */
 
 /* <Abre menu desplegable> */
 function loadInformation(){
@@ -15,7 +47,6 @@ function exit(){
 }
 /* </Oculta menu desplegable> */
 
-loadCarousel();
 document.documentElement.style.setProperty('--vh', `${window.innerHeight * 0.01}px`);
 document.documentElement.style.setProperty('--vhprog', `${window.innerHeight * 0.01}px`);
 window.addEventListener('resize', () => { document.documentElement.style.setProperty('--vhprog', `${window.innerHeight * 0.01}px`); });
